33126b29e3db757278a61fca1f4f48c4921d02fa
Stedd <tor.andre.paulsen@gmail.com>
2021-09-19 00:17:55 +0200

    Enemy & Weaopn update Health Receive damage on hit Death critetia

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,9 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject damageVFX;
     [SerializeField] Transform parent;
+    [SerializeField] float health = 10f;
     [SerializeField] int scoreValue = 1;
 
     ScoreBoard scoreBoard;
@@ -19,13 +21,20 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        ProcessHit();
-        ProcessDeath(other);
+        ProcessHit(other);
     }
 
-    private void ProcessHit()
+    private void ProcessHit(GameObject other)
     {
+        GameObject vfx = Instantiate(damageVFX, transform.position, Quaternion.identity);
         scoreBoard.UpdateScore(scoreValue);
+        health -= other.GetComponent<Weapon>().damage;
+        Debug.Log($"{name} received {other.GetComponent<Weapon>().damage} damage. now has {health} left");
+        if(health <= 0)
+        {
+            Debug.Log($"{name} died");
+            ProcessDeath(other);
+        }
     }
 
     private void ProcessDeath(GameObject other)
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] public float damage = 1f;
+}
