6107c468f54cf4950fe42e7c4f16c8f467b177fb
Stedd <tor.andre.paulsen@gmail.com>
2021-09-18 22:34:20 +0200

    Enemy explosions Instantiation and Destruction

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
     private void OnParticleCollision(GameObject other)
     {
         Debug.Log($"{name} destroyed by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
