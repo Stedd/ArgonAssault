7f10934a5c0abad0a7353afc421d071ac73f8a04
Stedd <tor.andre.paulsen@gmail.com>
2021-09-18 22:07:31 +0200

    Added explosion and disappear on collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    Mover mover;
+
+    [Header("Particles")]
+    [SerializeField] GameObject[] particles;
+    [SerializeField] GameObject[] parts;
+
+    private void Start()
     {
-        Debug.Log(this.name + " --collided with-- " + collision.gameObject.name);
+        mover = GetComponent<Mover>();
     }
+
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log($"{this.name} **triggered by** {collision.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        Debug.Log("Crashed, respawning");
+        PlayExplosion();
+        DisableMovement();
+        DisableBoxCollider();
+        Invoke("Respawn", 1f);
+    }
+
+
+    void PlayExplosion()
+    {
+        particles[0].GetComponent<ParticleSystem>().Play();
+        
+        foreach (GameObject part in parts)
+        {
+            var meshRenderer = part.GetComponent<MeshRenderer>();
+            meshRenderer.enabled = false;
+        }
+        //gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    void DisableMovement()
+    {
+        mover.enabled = false;
+    }
+
+    private void DisableBoxCollider()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+    }
+
+    void Respawn()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
