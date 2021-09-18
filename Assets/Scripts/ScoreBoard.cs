e139008913630013297ad65c28c6559002ba6b56
Stedd <tor.andre.paulsen@gmail.com>
2021-09-18 23:28:07 +0200

    Score Text

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
+    TMP_Text scoreText;
 
     int score;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        UpdateScoreText(score);
+    }
 
     public void UpdateScore(int _amount)
     {
         score += _amount;
-        Debug.Log($"Score: {score}");
+        UpdateScoreText(score);
+    }
+
+    private void UpdateScoreText(int _score)
+    {
+        scoreText.text = $"Score: {score}";
     }
 }
